e66be830ca3f8b7cf1c64bb8323215c93346f148
quan tran <tranmqt@gmail.com>
2018-09-23 22:02:14 -0700

    added boilerplate Flask code with comments and example Test endpoint

---
+++ app.py
@@ -0,0 +1,86 @@
+"""This is the main driver and entrypoint for the polyrides API.
+
+This file provides functionality for setting up and running our Flask app.
+"""
+import argparse
+
+from flask import Flask, jsonify
+from flask_restful import Api, Resource
+
+from polyrides.routes.test import Test
+
+
+class Index(Resource):
+    """An example Resource to be attached to the root ('/') endpoint of our Flask app.
+
+    All it does is return a message in response to an HTTP GET request.
+    """
+    def __init__(self):
+        """Initialize the Resource, setting the default message.
+
+        This method is called every time the endpoint receives a request.
+        """
+        print('Called Index.__init__.')
+        self._msg = "Hello World!"
+
+    def get(self):
+        """Respond to a get request on this endpoint, returning this resource's message as JSON."""
+        return jsonify(self._msg)
+
+
+def _parse_args() -> argparse.Namespace:
+    """... Parse arguments."""
+    parser = argparse.ArgumentParser(description="")
+    parser.add_argument('--debug',
+                        help="Start the app in debug mode.",
+                        action='store_true',
+                        default=False)
+    parser.add_argument('--example_value',
+                        help="Passe an example value to this application. It doesn't do anything, but this illustrates how arguments are accessed.")
+    return parser.parse_args()
+
+
+def main():
+    """This is the main function. It runs the Flask app."""
+
+    # Parse the arguments passed to the command running this application.
+    args = _parse_args()
+
+    # This is how arguments are retrieved from the Namespace object returned by parse_args.
+    if args.example_value is not None:
+        my_value = args.example_value
+        print('You passed `{}` as the example value!'.format(my_value))
+
+    # Construct an instance of a Flask app. This app will have all of the functionality provided by Flask.
+    app = Flask(__name__)
+
+    # This configures the app's `debug` mode, which is an option provided as a command line argument.
+    # Debug mode will re-load the server when it detects a change in the source files and will give
+    # helpful messages when something breaks.
+    # Of course, we don't want these messages to appear in production since it will reveal details
+    # about how our code works to end users.
+    app_args = {
+        'debug': args.debug
+    }
+
+    # These are keyword args passed to the flask_restful.Api constructor.
+    # In this case, it just tells the app to handle all 404 errors for us.
+    api_args = {
+        'catch_all_404s': True
+    }
+
+    # Construct the REST API object attached to our Flask app defined above.
+    api = Api(app, **api_args)
+
+    # Set the Index resource we defined above as the handler for requests to the {base_url}.
+    api.add_resource(Index, '/')
+    # Set the Test resource defined in routes/test.py as the handler for all requests to {base_url}/test.
+    api.add_resource(Test, '/test')
+    # When a new resource is created, it must be added here so that our app knows about it.
+
+    # With setup complete, run the application.
+    app.run(**app_args)
+
+
+if __name__ == '__main__':
+    main()
---
+++ polyrides/routes/test.py
@@ -0,0 +1,115 @@
+"""Endpoint for retrieving test data.
+
+This class is a Resource mounted at {base_url}/test in our app's main function.
+"""
+import json
+
+from typing import Dict, List
+
+from flask import jsonify
+from flask_restful import reqparse, Resource
+
+from ..utils.utils import load_json, root_join
+
+
+def _parse_request() -> Dict:
+    """Parse the JSON body of an HTTP POST request.
+
+    When this function is called, it parses a global (but local to the context) variable called
+    `request`. Performing the parsing in this function keeps the code from being cluttered by
+    references to a `request` variable.
+
+    Returns:
+        Data from a request parsed according to the arguments defined in this function.
+        For example, a dict of the following format would be returned (unless this function has
+        changed since I wrote it.)
+        {
+            'id': 1,
+            'msg': 'Oliver likes cats'
+        }
+    """
+    parser = reqparse.RequestParser()
+    parser.add_argument('id', type=int, help="A unique integer ID.")
+    parser.add_argument('msg', type=str, help="A string message.")
+    # parser.add_argumemt('name_of_field', type=str, help="This is how you would add another argument.")
+    return parser.parse_args()
+
+
+class Test(Resource):
+    """Flask RESTful resource for interacting with test data.
+
+    This class extends the flash_restful.Resource class and is instantiated in app.py in the root
+    directory. This should be the exclusive point of entry for all functionality provided by the
+    {base_url}/test/ endpoint.
+    """
+
+    def __init__(self):
+        """Initialize the Resource, loading data from the source (which in this case is a JSON file).
+
+        This method is called every time the endpoint receives a request.
+        """
+        print('Called Test.__init__.')
+        self._data_file = root_join('data', 'test.json')
+        self.data = load_json(self._data_file)
+
+    def _validate(self, item: Dict) -> bool:
+        """Check if the given Test item has valid fields and a unique ID.
+
+        In an actual situation you would want to raise an Exception or provide more data, but this
+        is just an example so roll with it.
+
+        Args:
+            item (dict): The data parsed from a POST request.
+
+        Returns:
+            bool: True if the Test item can be added to the collection. False otherwise.
+        """
+        item_id = item['id']
+
+        if not item_id:
+            return False
+        if not item['msg']:
+            return False
+        # Check if the item ID is not unique.
+        if item_id in (saved_item['id'] for saved_item in self.data):
+            return False
+
+        return True
+
+    def _write(self, data: List):
+        """Serialize data, persisting it to some permanent storage medium.
+
+        In this example, we are serializing data locally in a JSON file. This is a bad idea. IRL,
+        we would use a database connection. Maybe SQL, maybe not. But probably SQL.
+        We are only doing this serialization to JSON thing to illustrate the idea of saving data
+        and mostly because it doesn't require any setup.
+
+        Args:
+            data (list): The list of test items we are writing to disk.
+        """
+        with open(self._data_file, 'w') as t_f:
+            t_f.write(json.dumps(data, indent=4))
+
+    def get(self):
+        """A GET request retrieves a document from the API.
+
+        In this case, a GET request to the base of this endpoint will retrieve all items tracked
+        by this resource.
+        """
+        return jsonify(self.data)  # By default, return HTTP Status 200, indicating 'OK'
+
+    def post(self):
+        """A POST request to the API creates a document.
+
+        This endpoint expects the request to contain an object to add to Test collection.
+        """
+        item = _parse_request()
+        # If the item matches the expected format, add it to the collection and save it.
+        if self._validate(item):
+            # Add the item to the data and sort by ID.
+            updated_data = sorted(self.data + [item], key=lambda item: item['id'])
+            self._write(updated_data)
+            return '', 201  # Return an empty response with HTTP Status 201, indicating 'Created'
+
+        # Return HTTP Status 400, indicating 'Bad Request'
+        return 'Boo, your request sucks, no one likes it', 400

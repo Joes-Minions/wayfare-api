980cf25031a13652e3f8142fd1f444d4dfc850b3
oliverwang101 <oliverlw1336@gmail.com>
2018-11-04 18:14:09 -0800

    updated ride model for relational mapping to user

--- polyrides/__init__.py
+++ polyrides/__init__.py
@@ -14,6 +14,7 @@ app = Flask(__name__)  # pylint: disable=C0103
 app.config['SQLALCHEMY_DATABASE_URI'] = DB_URI
 db = flask_sqlalchemy.SQLAlchemy(app)  # pylint: disable=C0103
 
+from polyrides.models.ride import Ride
 from polyrides.models.user import User  # pylint: disable=C0413
 # Necessary for sqlite
 db.drop_all()

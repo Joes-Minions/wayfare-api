b84503cf54d111c5604c5b6031d6ec0aaed14ff7
quan tran <tranmqt@gmail.com>
2018-11-08 21:15:00 -0800

    add MissingField exception

--- polyrides/exceptions.py
+++ polyrides/exceptions.py
@@ -11,3 +11,16 @@ class DuplicateEmail(Exception):
         self.email = email
         self.message = "Duplicate email: '{}'".format(email)
         super().__init__(self.message)
+
+
+class MissingField(Exception):
+    """Exception raised when a request body is missing a required field.
+
+    Attributes:
+        field (str): Name of missing field.
+    """
+    def __init__(self, field: str):
+        """Init MissingField with the given field_name."""
+        self.field = field
+        self.message = "Missing field: '{}'".format(field)
+        super().__init__(self.message)

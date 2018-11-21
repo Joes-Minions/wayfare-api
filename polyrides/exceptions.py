c8b740b0ebb3c1e6d79231748755cc8cda3ec035
Jiwon Lee <jiwonlee@Jiwons-MBP.lan>
2018-11-21 15:56:35 -0800

    validation exceptions updated

--- polyrides/exceptions.py
+++ polyrides/exceptions.py
@@ -1,10 +1,8 @@
 """Exceptions raised by PolyRides API routes and models."""
 
-
 class PolyRidesError(Exception):
     """Base Exception class for all user-defined errors."""
     pass
-
 
 class DuplicateEmailError(PolyRidesError):
     """Exception indicating that a `User` email already exists.
@@ -18,7 +16,6 @@ class DuplicateEmailError(PolyRidesError):
         self.message = "Duplicate email: '{}'".format(email)
         super().__init__(self.message)
 
-
 class InvalidEmailError(PolyRidesError):
     """Exception raised when a provided email value is invalid. 
 
@@ -29,4 +26,20 @@ class InvalidEmailError(PolyRidesError):
         """Init `DuplicateEmailError` with the given email."""
         self.email = email
         self.message = "Invalid email: '{}'".format(email)
+        super().__init__(self.message)
+
+class InvalidFirstNameError(PolyRidesError):
+    """Exception raised when length of provided first name is invalid.""" 
+
+    def __init__(self, first_name: str):
+        self.first_name = first_name
+        self.message = "Invalid first name: '{}'".format(first_name)
+        super().__init__(self.message)
+
+class InvalidLastNameError(PolyRidesError):
+    """Exception raised when length of provided last name is invalid.""" 
+
+    def __init__(self, last_name: str):
+        self.last_name = last_name
+        self.message = "Invalid last name: '{}'".format(last_name)
         super().__init__(self.message)

dcfa7d791f48ade39c9ead1d14586c8a1ef5bade
Jiwon Lee <jiwonlee@Jiwons-MBP.lan>
2018-11-21 14:35:25 -0800

    user name validation method added

--- polyrides/models/user.py
+++ polyrides/models/user.py
@@ -32,9 +32,34 @@ class User(AbstractModelBase):
             first_name (str): Value provided to field.
 
         Raises:
-            TODO
+            'InvalidFirstNameError': If the length of given first name is longer than 64
         """
+        if len(first_name) > 64:
+            raise InvalidFirstNameError(first_name)
+
+        if not re.compile('[~!@#$%^&*()+=_`]').search(first_name) == None:
+            raise InvalidFirstNameError(first_name)
+        
         return first_name
+
+    @db.validates('last_name')
+    def validate_last_name(self, key: str, last_name: str):
+        """Check that a first name is valid.
+
+        Args:
+            key (str): Dict key matching the validated field.
+            last_name (str): Value provided to field.
+
+        Raises:
+            'InvalidLastNameError': If the length of given first name is longer than 64
+        """
+        if len(last_name) > 64:
+            raise InvalidLastNameError(last_name)
+
+        if not re.compile('[~!@#$%^&*()+=_`]').search(last_name) == None:
+            raise InvalidLastNameError(last_name)
+
+        return last_name
 
     @db.validates('email')
     def validate_email(self, key: str, email: str):
@@ -51,11 +76,40 @@ class User(AbstractModelBase):
             `InvalidEmailError`: If the given email does not have exactly one '@' and a '.' after the '@'.
             `DuplicateEmailError`: If a user with the given email already exists.
         """
+
+        """
+        ^@ = any char except @
+        \ = inhibit the specialness of character
+        https://developers.google.com/edu/python/regular-expressions
+        """
         if not re.compile(r'[^@]+@[^@]+\.[^@]+').match(email):
             raise InvalidEmailError(email)
+
         if self.find_by_email(email):
             raise DuplicateEmailError(email)
+
         return email
+
+    @db.converts('name')
+    def convert_name(self, name: str):
+        """Will be called after name is validated (no special char except '-').
+
+        Args:
+            name (str): name that will be converted into lower case.
+        
+        Return:
+            str: name converted into lower case.
+
+        """
+        name = name.split("-")
+        res = []
+
+        for x in name:
+            res.append(x.lower())
+
+        joined = "-".join(res)
+
+        return joined
 
     @staticmethod
     def find_by_id(user_id: int) -> UserType:
